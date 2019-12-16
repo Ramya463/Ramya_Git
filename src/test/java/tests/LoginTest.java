62ccbc10d1c6b508a640000eb363bdc469ad7be4
Ramya <panguluri_ramya@yahoo.co.in>
2019-12-16 11:51:36 +0530

    push latest code

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -26,13 +26,13 @@ public class LoginTest extends SuiteManager {
         String pword=config.getProperty("password");
         String searchValue=config.getProperty("searchitem");
 
-       homePage=loginPage.login(user_name,pword);
-       homePage.searchItem(searchValue);
-       homePage.print_noofprod();
-       homePage.check_products();
-       addCartPage=homePage.cartPage();
-       checkOut=addCartPage.Click_AddtoCart();
-       checkOut.click_checkout();
+       homePage=loginPage.login(user_name,pword);//logging in with username and password
+       homePage.searchItem(searchValue);//Search using a keyword
+       homePage.print_noofprod();//Printing number of products
+       homePage.check_products();//validating the products
+       addCartPage=homePage.cartPage();//getting the cart page
+       checkOut=addCartPage.Click_AddtoCart();//clicking on add cart and move to cart details page
+       checkOut.click_checkout();//check out of cart items
 
 
 
